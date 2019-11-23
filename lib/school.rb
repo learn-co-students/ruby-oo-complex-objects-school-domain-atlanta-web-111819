3359e26d7bfab90ab5bdd3b7a5cbe18f6f53593b
Javi <javsteratii@gmail.com>
2019-11-23 12:32:03 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+
+class School 
+    #this should be reffered to as a 
+    #model in the domain model context of code.
+    attr_accessor :roster 
+
+    def initialize(roster)
+        @roster = {}
+        @sort = {}
+    end 
+
+    def add_student(name, grade)
+        # students = @roster[grade]= []
+       if @roster.has_key?(grade)
+            @roster[grade] << name 
+       else 
+            @roster[grade]= []
+            @roster[grade] << name
+       end 
+    end 
+
+    def grade(grade)
+        @roster[grade]
+    end 
+
+    def sort
+        @roster.select {|k,v| v.sort!}
+    end 
+
+end
