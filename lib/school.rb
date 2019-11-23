b1262bc11c3f56fb3b22484db23dbecf0d3b2c43
Javi <javsteratii@gmail.com>
2019-11-23 12:39:51 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,5 @@
 # code here!
-
+require 'pry'
 class School 
     #this should be reffered to as a 
     #model in the domain model context of code.
@@ -25,6 +25,11 @@ class School
     end 
 
     def sort
+        #@roster.select {|k,v| v.sort}
+        # ^^^ needs the .sort! bc it returns self sorted
+        # .sort returns new array which doesnt modify
+        #the roster by sorting them... it would return a new
+        #array of values from roster. 
         @roster.select {|k,v| v.sort!}
     end 
 
